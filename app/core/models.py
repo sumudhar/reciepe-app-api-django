a5152d5d78ade729ca1e85af72489e5da8b9b17c
mtirupat <mtirupat@cisco.com>
2020-08-10 16:27:37 +0530

    flake dependency removed

--- app/core/models.py
+++ app/core/models.py
@@ -1,17 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import  AbstractBaseUser, BaseUserManager,\
-                                        PermissionsMixin
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
 
+
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password= None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """ Creates and saves New User"""
         if not email:
             raise ValueError('Users must have an email address')
-        user= self.model(email= self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -26,16 +27,14 @@ class UserManager(BaseUserManager):
 
         return user
 
-class User(AbstractBaseUser,PermissionsMixin):
+
+class User(AbstractBaseUser, PermissionsMixin):
     """ Custom user model that supports using email instead of username"""
-    email= models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active=models.BooleanField(default=True)
-    is_staff= models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-    objects= UserManager()
+    objects = UserManager()
 
-    USERNAME_FIELD= "email"
-
-
-
+    USERNAME_FIELD = "email"   
