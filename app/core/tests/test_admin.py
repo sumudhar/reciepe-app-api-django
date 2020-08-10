2ae54c5867577da1af2e60934369095c781864de
mtirupat <mtirupat@cisco.com>
2020-08-10 16:44:56 +0530

    flake dependency removed

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -20,7 +20,7 @@ class AdminSiteTests(TestCase):
         )
 
     def test_users_listed(self):
-        url = reverse('admin:core_user_changelist')
+        url = reverse("admin:core_user_changelist")
         res = self.client.get(url)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
